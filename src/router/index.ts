eff18df5f0358f99589c6fcca9accb7b0ee050c8
Siddhesh Mangela <sidthecool007@gmail.com>
2021-08-25 11:32:40 +0530

    routes fixed

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,18 +2,16 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 
 const pages = import.meta.globEager('../pages/*.vue');
 
-const routes: Array<RouteRecordRaw> = Object.entries(pages).map(
-  ([nameRaw, module]) => {
-    const name = nameRaw.replace('../pages/', '').replace('.vue', '');
-    const path = name === 'master' ? '/' : `/${name}`;
+const routes: Array<RouteRecordRaw> = Object.entries(pages).map(([nameRaw]) => {
+  const name = nameRaw.replace('../pages/', '').replace('.vue', '');
+  const path = name === 'master' ? '/' : `/${name}`;
 
-    return {
-      name,
-      path,
-      component: module.default,
-    };
-  },
-);
+  return {
+    name,
+    path,
+    component: () => import(`../pages/${name}.vue`),
+  };
+});
 
 const router = createRouter({
   history: createWebHistory(),
